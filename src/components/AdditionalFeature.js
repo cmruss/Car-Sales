77a6803e0db7126d9663a3e4e148f50b8a91b5b3
cmruss <cmrussell08@gmail.com>
2019-12-11 18:50:25 -0500

    MVP

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addFeature } from '../actions/vehicleActions'
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={()=>{props.addFeature(props.feature)}} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  };
+};
+
+export default connect(mapStateToProps, {addFeature})(AdditionalFeature);
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AdditionalFeature from './AdditionalFeature';
+import { FeatureContext } from '../contexts/FeatureContext';
 
 const AdditionalFeatures = props => {
+  const { addFeature } = useContext(FeatureContext);
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
           {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addFeature={addFeature} />
           ))}
         </ol>
       ) : (
